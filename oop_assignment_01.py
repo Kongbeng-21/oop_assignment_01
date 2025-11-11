3f901e9a9962f884f44d0a2d7a1192dabcef6ded
Krittitee Chaisang <kongbeng21@gmail.com>
2025-11-11 15:44:24 +0700

    commit 2 class member

--- oop_assignment_01.py
+++ oop_assignment_01.py
@@ -15,3 +15,20 @@ class Book:
             'available_copies':self.available_copies
         }
         return book
+    
+class Member:
+    def __init__(self,id, name, email, borrowed_books_list):
+        self.id =id
+        self.name = name 
+        self.email = email
+        self.borrowed_books_list = borrowed_books_list
+        
+    def template(self):
+        member = {
+            'id':self.id,
+            'name':self.name,
+            'email':self.email,
+            'borrowed_books_list':self.borrowed_books_list
+            
+        }
+        return member
